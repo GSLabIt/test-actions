db5f8f393489ad1c92c64d10197fcd9233a41cf4
Giovanni <giovanni@gslab.it>
2020-06-02 12:32:28 +0200

    code clean

--- .github/actions/get-workflow-id-action/main.py
+++ .github/actions/get-workflow-id-action/main.py
@@ -3,10 +3,8 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
-    print('sticaz , access_token')
     token = os.environ.get("INPUT_ACCESS_TOKEN")
     repo = os.environ.get("GITHUB_REPOSITORY")
-    print('sticaz 2', token, repo, os.environ),
     if not token and repo:
         return 1
     headers = {'Authorization': 'token ' + token}
@@ -14,9 +12,7 @@ def main():
     r = requests.get(url, headers=headers)
     if r.status_code != requests.codes.ok:
         return 1
-    print(url, r.json())
     workflow_id = r.json().get('id', False)
-    print('workflow_id', workflow_id)
     print(f"::set-output name=workflow_id::{workflow_id}")
     return 0
 
