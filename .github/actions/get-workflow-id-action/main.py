d37ff611b96824243af552f480f11b4a9022c16c
Giovanni <giovanni@gslab.it>
2020-06-02 11:41:32 +0200

    multiple fix

--- .github/actions/get-workflow-id-action/main.py
+++ .github/actions/get-workflow-id-action/main.py
@@ -3,18 +3,18 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
-    workflow_file_name = os.environ.get("GITHUB_WORKFLOW", False)
     token = os.environ.get("GITHUB_TOKEN", False)
     repo = os.environ.get("GITHUB_REPOSITORY", False)
-    print(workflow_file_name)
-    if workflow_file_name and token:
-        headers = {'Authorization': 'token ' + token}
-        url = 'https://api.github.com/repos/{0}/actions/workflows'.format(repo)
-        response = requests.get(url, headers=headers)
-        if response != requests.codes.ok:
-            return 1
-        workflow_id = response.json().get('workflows')[0].get('id', False)
-        print(f"::set-output name=WORKFLOW_ID::{workflow_id}")
+    if not token and repo:
+        return 1
+    headers = {'Authorization': 'token ' + token}
+    url = f'https://api.github.com/repos/{repo}/actions/workflows/build.yml'
+    response = requests.get(url, headers=headers)
+    print(url, response.json())
+    if response != requests.codes.ok:
+        return 1
+    workflow_id = response.json().get('workflows')[0].get('id', False)
+    print(f"::set-output name=WORKFLOW_ID::{workflow_id}")
     return 0
 
 
