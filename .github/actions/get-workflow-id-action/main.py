e0beba1b3d03ca34cc6658e173418716a53d99df
Giovanni <giovanni@gslab.it>
2020-06-02 15:50:00 +0200

    fix file name

--- .github/actions/get-workflow-id-action/main.py
+++ .github/actions/get-workflow-id-action/main.py
@@ -5,10 +5,11 @@ import requests  # noqa We are just importing this to prove the dependency insta
 def main():
     token = os.environ.get("INPUT_ACCESS_TOKEN")
     repo = os.environ.get("GITHUB_REPOSITORY")
+    file_name = os.environ.get("GITHUB_WORKFLOW_FILE_NAME")
     if not token and repo:
         return 1
     headers = {'Authorization': 'token ' + token}
-    url = f'https://api.github.com/repos/{repo}/actions/workflows/build.yml'
+    url = f'https://api.github.com/repos/{repo}/actions/workflows/{file_name}'
     r = requests.get(url, headers=headers)
     if r.status_code != requests.codes.ok:
         return 1
