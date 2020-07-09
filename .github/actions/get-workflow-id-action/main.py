fea5a1e8e044df113b58753b12dc74a5b3b4c4cc
Giovanni <giovanni@gslab.it>
2020-07-09 19:21:36 +0200

    - new actions

--- .github/actions/get-workflow-id-action/main.py
+++ .github/actions/get-workflow-id-action/main.py
@@ -1,6 +1,7 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
 import sys
+
+import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 
 def main():
@@ -9,13 +10,12 @@ def main():
     file_name = os.environ.get("INPUT_WORKFLOW_FILE_NAME")
     if not (token and repo and file_name):
         return 1
-    headers = {'Authorization': 'token ' + token}
-    url = f'https://api.github.com/repos/{repo}/actions/workflows/{file_name}'
+    headers = {"Authorization": "token " + token}
+    url = f"https://api.github.com/repos/{repo}/actions/workflows/{file_name}"
     r = requests.get(url, headers=headers)
     if r.status_code != requests.codes.ok:
         return 1
-    workflow_id = r.json().get('id', False)
-    print('workflow_id', workflow_id)
+    workflow_id = r.json().get("id", False)
     sys.stdout.write(f"::set-output name=workflow_id::{workflow_id}")
     return 0
 
